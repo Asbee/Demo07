0cffa4f5c59c4a917ede888caefadb3bbbcfd0e6
Asbee <Santtu_boman@hotmail.com>
2016-02-02 11:08:26 +0200

    Demo07

---
+++ KT2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+
+                StreamWriter outputfile = new StreamWriter(@"D:\K2845\Demo07\KT2\integersnumbers.txt");
+                StreamWriter outputfile2 = new StreamWriter(@":\K2845\Demo07\KT2\Doublenumbers.txt");
+                do
+                {
+                    Console.Write("Give number (enter ends): ");
+                    line = Console.ReadLine();
+                    Console.WriteLine(line);
+                    
+                } while (line != "");
+
+                double doubles;
+              bool.parsed =  int.TryParse(line, out doubles)
+                   
+                
+                outputfile.Close();
+            } catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            } catch (Exception)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+
+            
+            try
+            {
+
+                string[] text = File.ReadAllLines(@"D:\K2845\Demo07\KT2\numbers.txt");
+                foreach (string lines in text)
+                {
+                    Console.WriteLine(lines);
+                }
+            }catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+            
+             
+            
+            
+           
+
+        }
+    }
+}
---
+++ T1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            
+            StreamWriter outputfile = new StreamWriter(@"D:\K2845\Demo07\test.txt");
+
+            do
+            {
+                Console.Write("Write textlines: ");
+                line = Console.ReadLine();
+                outputfile.WriteLine(line);
+
+
+            } while (line != "");
+
+            outputfile.Close();
+            
+            try
+            {
+                
+                string[] text = File.ReadAllLines (@"D:\K2845\Demo07\test.txt");
+                foreach (string lines in text)
+                {
+                   Console.WriteLine(lines);
+                }
+                
+                
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+    }
+}
